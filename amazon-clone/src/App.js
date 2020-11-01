035c8006ce2101c6dba84333c9a6f87d607db88b
Wahidul hasan Abir <wahidpreeenon@gmail.com>
2020-11-01 17:30:51 +0600

    header

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM
     <div className="app">
       <h1>hello 🚀 </h1>
+      <Header />
+      {/* Header */}
+      {/* Home */}
     </div>
   );
 }
