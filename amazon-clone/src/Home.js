6bc6fc6d1f3009b8bf6df72e6b136330c8ff6097
Wahidul hasan Abir <wahidpreeenon@gmail.com>
2020-11-02 12:55:10 +0600

    component images

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
     return (
@@ -7,7 +8,7 @@ function Home() {
             <div className="home__container">
                 <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt=""/>
                 <div className="home__row">
-                    {/* Product */}
+                    <Product />
                     {/* Product */}
                 </div>
                 <div className="home__row">
