3c98a93bec2bb92aa28f6d6f91f495078d5c129c
wahidul-dev <wahidul.abir@northsouth.edu>
2020-11-23 20:43:04 +0600

    return n order component added

--- amazon-clone/src/CheckoutProduct.js
+++ amazon-clone/src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./CheckoutProduct.css";
 import { useStateValue } from './StateProvider';
 
-function CheckoutProduct({ id, image, title, price, rating}) {
+function CheckoutProduct({ id, image, title, price, rating, hideButton}) {
     const [{ basket }, dispatch] = useStateValue();
 
     const removeFromBasket = () => {
@@ -31,7 +31,10 @@ function CheckoutProduct({ id, image, title, price, rating}) {
                         <p>⭐</p>
                     ))}
                 </div>
-                <button onClick={removeFromBasket}>Remove From Basket</button>
+                {!hideButton && (
+                    <button onClick={removeFromBasket}>Remove From Basket</button>
+                )}
+                
 
             </div>
         </div>
--- amazon-clone/src/Order.js
+++ amazon-clone/src/Order.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Order.css";
 import moment from "moment";
 import CheckoutProduct from "./CheckoutProduct";
+import CurrencyFormat from 'react-currency-format';
 
 function Order({ order }) {
     return (
@@ -20,8 +21,19 @@ function Order({ order }) {
                     image={item.image}
                     price={item.price}
                     rating={item.rating}
+                    hideButton
                 />
             ))}
+            <CurrencyFormat
+                renderText={(value) => (
+                    <h3 className="order__total">Order Total: {value}</h3>
+                )}
+                decimalScale={2}
+                value={order.data.amount / 100}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+            />
         </div>
     )
 }
