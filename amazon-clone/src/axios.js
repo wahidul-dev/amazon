54a68ffa903539103eae579f568d7f72dd92b95a
wahidul-dev <wahidul.abir@northsouth.edu>
2020-11-27 21:35:25 +0600

    deployed the web app

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/clone-3c25a/us-central1/api" // THE API (cloud function) URL
+    // THE API (cloud function) URL
+    baseURL: "https://us-central1-clone-3c25a.cloudfunctions.net/api" 
 });
 
 export default instance;
+
+// https://us-central1-clone-3c25a.cloudfunctions.net/api
+
+// http://localhost:5001/clone-3c25a/us-central1/api
